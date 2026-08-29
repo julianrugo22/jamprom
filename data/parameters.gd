7de1f9eaa7d209a4ab6a04b71ff86bd0d3c64114
Suspicious-Garbage <70545246+Suspicious-Garbage@users.noreply.github.com>
2026-08-29 11:38:08 -0300

    Add files via upload

---
+++ data/parameters.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var MIN_RADIUS: float = 30
+var MAX_RADIUS: float = 30
+var MAX_MOMENTUM: float = 1000000000000000
+var STARTING_MOMENTUM: float = 300
+var ACCELERATION: float = 10
+var DECCELERATION: float = 0
